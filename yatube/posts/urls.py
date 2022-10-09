84251aa81d96937d2e74af7332bf1e31370a5f07
Анатолий Черножуков <sir.kalashnik@yandex.ru>
2022-10-10 02:46:27 +0300

    Добавлены urls и view-функции

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -4,5 +4,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #Главная страница
     path('', views.index),
+    #Посты, отфильтрованные по группам
+    path('group/<slug:slug>/', views.group_posts),
 ] 
